593ab909a0e5f4ffab016a375a54dfaa26d08251
Charles Poulsen <charles.poulsen@gmail.com>
2023-04-08 14:02:36 +0800

    First commit - generated code from OpenAI ChatGPT GPT-4

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+from scipy.stats import norm
+import math
+
+app = Flask(__name__)
+
+def black_scholes(stock_price, strike_price, time_to_expiration, risk_free_rate, implied_volatility, option_type):
+    d1 = (math.log(stock_price / strike_price) + (risk_free_rate + 0.5 * implied_volatility ** 2) * time_to_expiration) / (implied_volatility * math.sqrt(time_to_expiration))
+    d2 = d1 - implied_volatility * math.sqrt(time_to_expiration)
+
+    if option_type.lower() == 'call':
+        price = stock_price * norm.cdf(d1) - strike_price * math.exp(-risk_free_rate * time_to_expiration) * norm.cdf(d2)
+    elif option_type.lower() == 'put':
+        price = strike_price * math.exp(-risk_free_rate * time_to_expiration) * norm.cdf(-d2) - stock_price * norm.cdf(-d1)
+    else:
+        raise ValueError('Invalid option type. Use "call" or "put".')
+
+    return price
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        stock_price = float(request.form['stock_price'])
+        strike_price = float(request.form['strike_price'])
+        time_to_expiration = float(request.form['time_to_expiration']) / 365
+        risk_free_rate = float(request.form['risk_free_rate']) / 100
+        implied_volatility = float(request.form['implied_volatility']) / 100
+        option_type = request.form['option_type']
+
+        option_price = black_scholes(stock_price, strike_price, time_to_expiration, risk_free_rate, implied_volatility, option_type)
+        return render_template('index.html', option_price=option_price)
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
